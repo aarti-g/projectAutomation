819fcdb878df4dd4f18d286011d46e64688ee4fc
aarti-g <78338785+aarti-g@users.noreply.github.com>
2021-02-01 20:57:29 +0530

    Update demo1.java

--- qsp/src/automation1/demo1.java
+++ qsp/src/automation1/demo1.java
@@ -7,9 +7,9 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class demo1 {
 
 	public static void main(String[] args) {
- //System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
-  //new ChromeDriver();
-		System.setProperty("webdriver.gecko.driver","./drivers/geckodriver.exe");
-		WebDriver driver=new FirefoxDriver();	} 
+ System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
+ new ChromeDriver();
+		//System.setProperty("webdriver.gecko.driver","./drivers/geckodriver.exe");
+		//WebDriver driver=new FirefoxDriver();	} 
 
 }
